be89c37864bb06bded78c482013175a6a955db80
LubabaSultana25 <tahiaanu.cpi@gmail.com>
2024-03-15 11:53:40 +0600

    proptype

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/Bookmarks'
 import Header from './Components/Header/Header'
 function App() {
- 
-  return (
+
+  return ( 
     <>
-      
-    <Header></Header>
-     <Blogs></Blogs>
-     
+
+      <Header></Header>
+      <div className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
+
     </>
   )
 }
